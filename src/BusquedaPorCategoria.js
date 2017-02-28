3ddf1a7d83263d8b69a0d90ce37a417ee38c97fb
JorgeBojaca <jorgebojaca@gmail.com>
2017-02-27 22:21:29 -0500

    Diseño en los resultados de busqueda por categoria

--- src/BusquedaPorCategoria.js
+++ src/BusquedaPorCategoria.js
@@ -7,11 +7,13 @@ class BusquedaPorCategoria extends Component {
   }
   componentDidMount(){
     var categoria = this.props.categoria;
-    // var uri = "https://api.mercadolibre.com/sites/MCO/search?category="+categoria+"&official_store_id=all";
-    var uri = "https://api.mercadolibre.com/sites/MCO/search?category=MCO1574&official_store_id=all";
+    console.log(categoria);
+    var uri = "https://api.mercadolibre.com/sites/MCO/search?category="+categoria+"&official_store_id=all";
+    // var uri = "https://api.mercadolibre.com/sites/MCO/search?category=MCO1574&official_store_id=all";
     fetch(uri)
     .then((response) => response.json())
     .then((responseJson) => {
+      console.log(responseJson.results);
       this.setState({items: responseJson.results});
     })
   }
@@ -20,8 +22,14 @@ class BusquedaPorCategoria extends Component {
     return (
       <div>
         <div className="contenido">
-          <h1>Items:</h1>
-          {this.state.items.map(item => <div>{item.title}</div>)}
+        <h2 className="resultados">Resultados:</h2>
+          {this.state.items.map(item => (
+            <div className="item">
+              <div className="titulo">{item.title}</div>
+              <div className="imagen"><img src={item.thumbnail}/></div>
+              <div className="precio">Precio: ${item.price}</div>
+              <div className="link"><a className="link" href={item.permalink}>Más información</a></div>
+            </div>))}
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import BusquedaPorCategoria from './BusquedaPorCategoria'
 import './index.css';
 
-const element = (
-  <div>
-    <div className="header">
-    </div>
-    <BusquedaPorCategoria />
-  </div>
-);
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+
+  var categoria = 'MCO1574';
+  ReactDOM.render(
+    <BusquedaPorCategoria categoria={categoria}/>,
+    document.getElementById('root')
+  );
