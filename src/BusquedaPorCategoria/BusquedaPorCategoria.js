32612b1230eb650f14568768d4263afbadd37fb8
JorgeBojaca <jorgebojaca@gmail.com>
2017-03-02 20:58:58 -0500

    Integración select con busqueda por categoria

--- src/BusquedaPorCategoria/BusquedaPorCategoria.js
+++ src/BusquedaPorCategoria/BusquedaPorCategoria.js
@@ -4,28 +4,38 @@ import Resultado from '../Resultado/Resultado';
 class BusquedaPorCategoria extends Component {
   constructor(props) {
     super(props);
-    this.state = {items: []};
+    this.state = {
+      items: [],
+      categorias: [],
+      categoria: ''
+    };
+
   }
-  componentDidMount(){
-    var categoria = this.props.categoria;
-    console.log(categoria);
-    
-  }
-      realizarPeticion(categoria) {
-        // let _this = this;
-        // fetch('https://api.mercadolibre.com/sites/MCO/search?q=' + texto).then(result => result.json()).then(resultado => {
-        //     _this.setState({resultados: resultado.results});
-        // }).catch(function (error) {
-        //     console.error(error);
-        //     return null;
-        // })
-        var uri = "https://api.mercadolibre.com/sites/MCO/search?category="+categoria+"&official_store_id=all";
-        fetch(uri)
-        .then((response) => response.json())
-        .then((responseJson) => {
-          console.log(responseJson.results);
-          this.setState({items: responseJson.results});
-        })
+
+
+   componentDidMount() {
+      this.state = {
+          categorias: []
+      };
+      let _this = this;
+      fetch('https://api.mercadolibre.com/sites/MCO/categories')
+      .then((response) => response.json())
+      .then((responseJson) => {
+            this.setState({categorias: responseJson});
+      }).catch(function(error) {
+          console.log(error);
+          return null;
+      })
+      //this.realizarPeticion('MCO40433');
+    }
+    realizarPeticion(categoria) {
+      this.setState({categoria: categoria});
+      var uri = "https://api.mercadolibre.com/sites/MCO/search?category="+categoria+"&official_store_id=all";
+      fetch(uri)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({items: responseJson.results});
+      })
 
 
     }
@@ -38,10 +48,19 @@ class BusquedaPorCategoria extends Component {
     return (
       <div>
         
-        <div className="contenido">
-        <h2 className="resultados">Resultados:</h2>
-          {this.state.items.map(item => (
-            <Resultado key={item.id} resultado={item} />))}
+        
+          <select value={this.state.categoria} onChange={this.cambioSelect.bind(this)}>
+            {this.state.categorias.map(categoria => <option value={categoria.id}>{categoria.name}</option>)}
+          </select>
+          <div className="contenido">
+            
+              {
+                console.log(this.state.items.length)
+              }
+              {
+              this.state.items.map(item => (
+              <Resultado key={item.id} resultado={item} />)
+              )}  
         </div>
       </div>
     );
@@ -49,3 +68,6 @@ class BusquedaPorCategoria extends Component {
 }
 
 export default BusquedaPorCategoria;
+
+
+
